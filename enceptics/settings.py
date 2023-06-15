897ec3f43143608c79b715e8e5b540f5bc7d19b5
Pascal-Owilly <pascalouma54@gmail.com>
2023-06-16 01:52:47 +0300

    u

--- enceptics/settings.py
+++ enceptics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=o-np1nsx&2dc-+)4u+x449_8j)8o!tsdhu5h(9^dc8&rp^a!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost:3000' ]
 
 
 # Application definition
